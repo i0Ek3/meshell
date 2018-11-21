c24ea5a1064a60dd06ed6bd9b5ddd612ff84fabf
i0Ek3 <kno30826@gmail.com>
2018-11-21 20:11:12 +0800

    Update install4mac.sh

--- install4mac.sh
+++ install4mac.sh
@@ -18,12 +18,20 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 
 # common sf install
 # brew cask install xxxx
-brew install gawk cmake llvm proxychains-ng python3 go java wget gpg ant maven tomcat jetty hub mysql redis postgresql gradle mpg123 git-flow
+brew install gawk ack ag autojump automake cmake llvm proxychains-ng python3 go java wget gpg ant maven tomcat jetty hub mysql redis postgresql gradle mpg123 git-flow zsh-autosuggestions 
 brew postinstall python3
+
+# fairfont
+brew tap caskroom/fonts
+brew cask install font-fira-code
 
 # ruby env rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB ; \curl -sSL https://get.rvm.io | bash -s stable
 source $HOME.rvm/scripts/rvm; rvm install ruby
+
+# rust
+curl https://sh.rustup.rs -sSf | sh
+
 
 # finder-go
 curl -fsSL https://raw.githubusercontent.com/onmyway133/FinderGo/master/install.sh | sh
